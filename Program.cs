5fc02938430cd7cd3540230eba0e9c5613fd20b0
suvarchala12 <bhumasuvarchala55@gmail.com>
2021-12-28 11:11:11 +0530

    uc1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("StackAndQueue");
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack & Queue!");
+            StackListNode<int> stackListNode = new StackListNode<int>();
+            stackListNode.Push(70);
+            stackListNode.Push(30);
+            stackListNode.Push(56);
+            stackListNode.Show();
+        }
+    }
+}
