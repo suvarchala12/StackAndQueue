877ff7ab56c96d055d3bb6d31cd717ea0b9e84dd
suvarchala12 <bhumasuvarchala55@gmail.com>
2021-12-28 11:20:28 +0530

    uc3queue

--- Program.cs
+++ Program.cs
@@ -8,14 +8,22 @@ namespace StackAndQueue
         {
             Console.WriteLine("Stack & Queue!");
             StackListNode<int> stackListNode = new StackListNode<int>();
-            stackListNode.Push(70);
-            stackListNode.Push(30);
             stackListNode.Push(56);
+            stackListNode.Push(30);
+            stackListNode.Push(70);
             stackListNode.Show();
             stackListNode.Pop();
             stackListNode.Show();
             stackListNode.Peek();
             stackListNode.Show();
+            Console.WriteLine("----------------------------------------");
+            QueueListNode<int> queueListNode = new QueueListNode<int>();
+            queueListNode.Enqueue(56);
+            queueListNode.Enqueue(30);
+            queueListNode.Enqueue(70);
+            Console.WriteLine(queueListNode.Size);
+            Console.WriteLine("----------------------------------------");
+
         }
     }
 }
